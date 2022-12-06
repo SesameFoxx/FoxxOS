9cb7edc042107d9c4d59066781faac3a2fccc247
SesameFoxx <sesamefoxx@outlook.com>
2022-12-06 15:07:28 +0800

    lab6 by Ray Rao 20301168
    lab6 by Ray Rao 20301168

--- os/src/config.rs
+++ os/src/config.rs
@@ -8,4 +8,7 @@ pub const MAX_APP_NUM: usize = 4;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
+pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
+pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
+
 pub const CLOCK_FREQ: usize = 12500000;
