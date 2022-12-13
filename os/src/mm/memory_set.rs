be31c573449e56176a37415235736a3bbb04f961
SesameFoxx <sesamefoxx@outlook.com>
2022-12-13 11:16:15 +0800

    lab7 step4 by Ray Rao 20301168
    lab7 step4 by Ray Rao 20301168

--- os/src/mm/memory_set.rs
+++ os/src/mm/memory_set.rs
@@ -63,10 +63,10 @@ impl MemorySet {
 
     pub fn remove_area_with_start_vpn(&mut self, start_vpn: VirtPageNum) {
         if let Some((idx, area)) = self.areas.iter_mut().enumerate()
-            .find(|(_, area)| area.vpn_range.get_start() == start_vpn) {
-            area.unmap(&mut self.page_table);
-            self.areas.remove(idx);
-        }
+        .find(|(_, area)| area.vpn_range.get_start() == start_vpn) {
+        area.unmap(&mut self.page_table);
+        self.areas.remove(idx);
+    }
     }
 
     fn push(&mut self, mut map_area: MapArea, data: Option<&[u8]>) {
